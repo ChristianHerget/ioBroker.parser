32b92a8c5f3fe2320cfe859441d0da41a8fd4654
ChristianHerget <christian@herget.email>
2022-04-28 17:01:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -324,7 +324,7 @@ function readLink(link, callback) {
             url: link,
             rejectUnauthorized: false,
             timeout: 60000,
-            sendImmediately: false
+            sendImmediately: true
         }, (error, response, body) => callback(!body ? error || JSON.stringify(response) : null, body, link));
     } else {
         path = path || require('path');
