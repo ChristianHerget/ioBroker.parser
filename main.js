b1d4928ea12c074852d0b0979bf0a6e7d923ed6b
ChristianHerget <christian@herget.email>
2022-04-28 14:24:55 +0200

    Debug

--- main.js
+++ main.js
@@ -329,8 +329,8 @@ function readLink(link, callback) {
                 rejectUnauthorized: false,
                 timeout: 60000,
                 auth: {
-                    user: "admin",
-                    pass: "admin",
+                    user: user,
+                    pass: pw,
                     sendImmediately: false
                 }
             };
@@ -338,6 +338,7 @@ function readLink(link, callback) {
             adapter.log.debug('Request URL: ' + arr[1] + arr[4]);
             adapter.log.debug('Request User: ' + user);
             adapter.log.debug('Request PW: ' + pw);
+            adapter.log.debug('Request: ' + requestParams);
         } else {
             requestParams = {
                 url: link,
